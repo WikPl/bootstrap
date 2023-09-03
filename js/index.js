33d924a595f6b6540fbc4d72c77102e776db8f96
Wiktor Plotast <wikplotast@gmail.com>
2023-09-03 12:35:24 +0200

    js data types

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-const p = document.createElement('p')
-p.textContent = 'Dynamicznie dodany paragraf'
-document.body.append(p)
+// string - 'Wiktor'
+const name = 'Wiktor'
+
+// number - 27
+const age = 27
+
+// boolean - true/false 
+
+// null & undefined 
+const test = null 
+let test2
+console.log(test);
+console.log(test2); 
