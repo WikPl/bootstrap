8b332b9bf7e3cdad807993bb84db74302e01dc8e
Wiktor Plotast <wikplotast@gmail.com>
2023-09-03 12:19:24 +0200

    what is DOM

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-const fav_color = 'red'
-const meal = 'schabowy'
-const favDrink1 = 'pepsi'
-const number = 123
-const name = "Wiktor"
-const age = 27
-
-console.log(`Cześć, jestem ${name} i mam ${age} lat.`)
+const p = document.createElement('p')
+p.textContent = 'Dynamicznie dodany paragraf'
+document.body.append(p)
