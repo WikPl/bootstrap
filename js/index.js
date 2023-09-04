b0a2a464e590fcb206bce1d8e3996073a5bfa7d5
Wiktor Plotast <wikplotast@gmail.com>
2023-09-04 18:52:47 +0200

    js task#2

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-// && = i
-// || = lub
-// ! = negacja
+// 1. Stwórz zmienną "SCORE". Zmienna "SCORE" ma przechowywać informację, czy 15 jest liczbą parzystą, czy nieparzystą.
 
-if (5 > 4 || 2 > 1) {
-	console.log('okay')
-} else {
-	console.log('not okay')
-}
+// Skorzystaj z modulo. Zmienną "SCORE" wyloguj w konsoli (powinna zostać wylogowana cyfra 1).
+
+
+
+
+// 2. Stwórz zmienną "NUM1" i przypisz do niej wartość 8.
+
+// Za pomocą inkrementacji, zwiększ wartość zmiennej "NUM1" do 10. Wynik wyloguj w konsoli.
+
+
+
+// 3. Stwórz zmienne "X" oraz "Y". Przypisz do nich liczby 5 oraz 10.
+
+// Następnie za pomocą operatorów przypisania (zapis skrócony), wypisz w konsoli wynik mnożenia oraz dzielenia "X" przez "Y".
+
+
+
+// P.S. Liczę na to, że przy punkcie 3 (i być może 2) pojawią się pewne problemy. Jeśli sobie z nimi poradzicie – genialnie. Jeśli nie – spokojnie, nie ma co się załamywać, ale dajcie z siebie wszystko! 💪
