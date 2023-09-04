ec605e0c3d07a38e1f64bff521a1f1b5ceafe6da
Wiktor Plotast <wikplotast@gmail.com>
2023-09-04 18:44:48 +0200

    comparison operators

--- js/index.js
+++ js/index.js
@@ -2,7 +2,19 @@ const x = 8
 const y = '8'
 const z = 9
 
-if (x === y) {
+if (x != y) {
+	console.log('ok')
+} else {
+	console.log('not ok')
+}
+
+if (x < z) {
+	console.log('ok')
+} else {
+	console.log('not ok')
+}
+
+if (x <= y) {
 	console.log('ok')
 } else {
 	console.log('not ok')
