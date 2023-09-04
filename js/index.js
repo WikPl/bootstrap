c716392db67b6802b1b3969e0b0ece6c218b02f8
Wiktor Plotast <wikplotast@gmail.com>
2023-09-04 18:48:06 +0200

    logical operators

--- js/index.js
+++ js/index.js
@@ -1,27 +1,4 @@
-const x = 8
-const y = '8'
-const z = 9
+// && = i
+// || = lub 
+// ! = negacja 
 
-if (x != y) {
-	console.log('ok')
-} else {
-	console.log('not ok')
-}
-
-if (x < z) {
-	console.log('ok')
-} else {
-	console.log('not ok')
-}
-
-if (y >= z) {
-	console.log('ok')
-} else {
-	console.log('not ok')
-}
-
-if (x !== y) {
-	console.log('ok')
-} else {
-	console.log('not ok')
-}
