54f5595e7ce38f2068787a6307fa4f405d6c10bf
Wiktor Plotast <wikplotast@gmail.com>
2023-09-11 19:13:05 +0200

    arrays

--- js/index.js
+++ js/index.js
@@ -1,11 +1,2 @@
-const passLength = 12
-
-if(passLength >= 10) {
-    console.log(`Masz bardzo dobre hasło`);
-}
-else if (passLength < 10 && passLength > 5) {
-    console.log(`Masz dobre hasło`);
-}
-else {
-    console.log(`Masz słabe hasło`);
-}
+const colors = ['red', 'green', 'blue']
+console.log(colors);
