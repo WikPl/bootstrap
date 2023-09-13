72b8ad6297a76ddc48de1400b2f33f9823e57fa4
Wiktor Plotast <wikplotast@gmail.com>
2023-09-13 19:16:38 +0200

    task3

--- js/index.js
+++ js/index.js
@@ -1,35 +1,4 @@
-function test(){
-    console.log('Moja pierwsza funkcja');
+const numbers = [1, 2, 3, 4, 5, 6]
+for (let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
 }
-
-test()
-
-function addNumbers(x, y){
-console.log(x + y);
-}
-
-addNumbers(4, 5)
-
-function substractNumbers(q, w){
-    console.log(q - w);
-}
-
-substractNumbers(10, 5)
-
-function multiplyNumbers(e, r){
-    console.log(e * r);
-}
-
-multiplyNumbers(2, 5)
-
-function divideNumbers(t, u){
-    console.log(t / u);
-}
-
-divideNumbers(10, 5)
-
-function showNameAgeFood(name, age, food) {
-    console.log(`Podane imię: ${name}, wiek: ${age}, jedzenie: ${food}.`);
-}
-
-showNameAgeFood('Wiktor', 28, 'kebab')
