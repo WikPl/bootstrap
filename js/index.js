ddad66c9803241a59f965356f56fe1a8250bc02f
Wiktor Plotast <wikplotast@gmail.com>
2023-09-13 18:58:16 +0200

    function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
-const fruits = ['banan', 'jabłko', 'pomarańcza', 'cytryna', 'arbuz']
-
-console.log(fruits);
-
-for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i]);
+function test(){
+    console.log('Moja pierwsza funkcja');
 }
+
+test()
