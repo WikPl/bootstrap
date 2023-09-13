de771108b23c8c05c4facd116d3de845a9ab23c9
Wiktor Plotast <wikplotast@gmail.com>
2023-09-13 19:24:15 +0200

    task3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
+console.log('Czytanie z tabeli:');
 const numbers = [1, 2, 3, 4, 5, 6]
+
 for (let i = 0; i < numbers.length; i++){
     console.log(numbers[i]);
 }
+
+console.log('Dodawanie trzech argumentów w funkcji:');
+function addArguments(arg1, arg2, arg3) {
+    console.log(arg1 + arg2 + arg3);
+}
+
+addArguments(1, 2, 3)
+
+console.log('Czy długość tablicy jest większa niż 5?');
+if (numbers.length > 5) {
+    console.log('TRUE');
+}
+else {
+    console.log('FALSE');
+}
+
+console.log(`Tablica z cyframi ma ${numbers.length} elementów.`);
