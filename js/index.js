6701f6cc3c6418df2447577dc4cf286d5aef460d
Wiktor Plotast <wikplotast@gmail.com>
2023-09-15 19:08:47 +0200

    querySelector

--- js/index.js
+++ js/index.js
@@ -1,23 +1,2 @@
-console.log('Czytanie z tabeli:');
-const numbers = [1, 2, 3, 4, 5, 6]
-
-for (let i = 0; i < numbers.length; i++){
-    console.log(numbers[i]);
-}
-
-console.log('Dodawanie i mnożenie trzech argumentów w funkcji:');
-function addMultiplyArguments(arg1, arg2, arg3) {
-    console.log(arg1 + arg2 * arg3);
-}
-
-addMultiplyArguments(1, 2, 3)
-
-console.log('Czy długość tablicy jest większa niż 5?');
-if (numbers.length > 5) {
-    console.log('TRUE');
-}
-else {
-    console.log('FALSE');
-}
-
-console.log(`Tablica z cyframi ma ${numbers.length} elementów.`);
+const p = document.querySelector('p')
+console.log(p)
