43cd9fa73698c2a4dde8c42e8e3b0ebd2aa5ce1d
Wiktor Plotast <wikplotast@gmail.com>
2023-09-15 19:29:40 +0200

    querySelectorAllloop

--- js/index.js
+++ js/index.js
@@ -2,4 +2,9 @@ const p = document.querySelector('#btn')
 console.log(p)
 
 const liItems = document.querySelectorAll('li')
-console.log(liItems);
+console.log(liItems[2]);
+
+const pAll = document.querySelectorAll('p')
+for(i = 0; i < pAll.length; i++) {
+console.log(pAll[i]);
+}
