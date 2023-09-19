129305cd25189b502ffd7a3e688cbcbab964bef5
Wiktor Plotast <wikplotast@gmail.com>
2023-09-19 18:50:58 +0200

    addEventListener_project

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
-const btn1 = document.querySelector('#btn1')
-const btn2 = document.querySelector('.btn2')
+const redBtn = document.querySelector('#btn1')
+const blueBtn = document.querySelector('.btn2')
 const square = document.querySelector('.color')
 const removeColorBtn = document.querySelector('.remove-color')
 
 function redColor(){
 	square.classList.add('red')
+	square.classList.remove('blue')
 } 
 
 function blueColor(){
 	square.classList.add('blue')
+	square.classList.remove('red')
 }
 
-btn1.addEventListener('click', redColor)
+redBtn.addEventListener('click', redColor)
 
-btn2.addEventListener('click', blueColor)
+blueBtn.addEventListener('click', blueColor)
