09a25cf20a58fad9d207ad31814d5f5885b9fa02
Wiktor Plotast <wikplotast@gmail.com>
2023-09-19 18:37:24 +0200

    addEventListener_project

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const btn1 = document.querySelector('#btn1')
-console.log(btn1)
-
 const btn2 = document.querySelector('.btn2')
-console.log(btn2);
-
 const square = document.querySelector('.color')
-console.log(square);
-
 const removeColorBtn = document.querySelector('.remove-color')
-console.log(remove_color);
+
+function redColor(){
+	square.classList.add('red')
+} 
+
+function blueColor(){
+	square.classList.add('blue')
+}
+
